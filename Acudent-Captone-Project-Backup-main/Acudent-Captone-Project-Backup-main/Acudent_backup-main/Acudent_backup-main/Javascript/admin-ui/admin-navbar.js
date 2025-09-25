7805a60ac153c8487eed8038c5f9c76bbf860fd9
theraux <161040552+theraux@users.noreply.github.com>
2025-09-26 00:18:25 +0800

    Add files via upload

---
+++ Acudent-Captone-Project-Backup-main/Acudent-Captone-Project-Backup-main/Acudent_backup-main/Acudent_backup-main/Javascript/admin-ui/admin-navbar.js
@@ -0,0 +1,5 @@
+const messageCount = 2;
+const notificationCount = 5;
+
+document.querySelector('.fa-envelope + .badge').textContent = messageCount;
+document.querySelector('.fa-bell + .badge').textContent = notificationCount;
